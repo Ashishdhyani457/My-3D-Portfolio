7b9975d9949550cfe3e0b80c17fa0b77f9205b10
Ashishdhyani457 <ashishdhyani457@gmail.com>
2024-03-02 00:04:01 +0530

    added tilt cards

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -17,8 +17,8 @@ const Computers = (isMobile) => {
       shadow-mapSize={1024}
       />
       <primitive object={computer.scene}
-      scale={isMobile?0.7:0.75}
-      position={isMobile?[0,-3,-2.2]:[0,-2.50,-1.5]}
+      scale={isMobile? 0.65 : 0.75}
+      position={isMobile? [0,-3,-2.2]:[0,-2.50,-1.5]}
       rotation={[-0.01,-0.2,-0.1]}
       />
        </mesh>
